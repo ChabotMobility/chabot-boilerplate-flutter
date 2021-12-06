8a92540ec3c6e05a26ca808958e08bed74bfb3b9
kh.kim <kh.kim@chabot.co.kr>
2021-12-06 11:24:41 +0900

    init

--- lib/src/page/main/main_page.dart
+++ lib/src/page/main/main_page.dart
@@ -1,5 +1,5 @@
-import 'package:channel_talk_flutter/channel_talk_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainView extends StatelessWidget {
   const MainView({
@@ -19,15 +19,22 @@ class MainView extends StatelessWidget {
           ListTile(
             title: Text('1. Channel Talk'),
             onTap: () async {
-              await ChannelTalk.boot(
-                pluginKey: 'dd82c5cc-12dc-403a-9368-21e04d44382b',
-                memberId: 'test', //B2C 고객 계정ID
-                memberHash: 'memberHash',
-                trackDefaultEvent: false,
-                hidePopup: false,
-                language: 'korean',
-              );
-              await ChannelTalk.openChat();
+              // await ChannelTalk.boot(
+              //   pluginKey: 'dd82c5cc-12dc-403a-9368-21e04d44382b',
+              //   memberId: 'test', //B2C 고객 계정ID
+              //   memberHash: 'memberHash',
+              //   trackDefaultEvent: false,
+              //   hidePopup: false,
+              //   language: 'korean',
+              // );
+              // await ChannelTalk.openChat();
+            },
+          ),
+          ListTile(
+            title: Text('1. Dynamic Link'),
+            subtitle: SelectableText('https://chabot.page.link/76UZ'),
+            onTap: () {
+              launch('https://chabot.page.link/76UZ?id=event');
             },
           )
         ],
